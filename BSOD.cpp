101205019f46ea65be482a6310a6a44b4b36d667
d4v1d <95427130+Amit19057120@users.noreply.github.com>
2022-11-20 01:31:53 +0530

    Update BSOD.cpp

--- BSOD.cpp
+++ BSOD.cpp
@@ -7,7 +7,7 @@ EXTERN_C NTSTATUS NTAPI RtlAdjustPrivilege(ULONG, BOOLEAN,BOOLEAN, PBOOLEAN);
 EXTERN_C NTSTATUS NTAPI NtRaiseHardError(NTSTATUS ErrorStatus, ULONG NumberOfParamerters, ULONG UnicodeStringParameterMask, PULONG_PTR Parameters, ULONG ValidResponseOption, PULONG Response);
 
 int main() {
-    bool bl;
+    BOOLEAN bl;
     unsigned long response;
     RtlAdjustPrivilege (19,true, false, &bl);
     NtRaiseHardError(STATUS_ASSERTION_FAILURE,0,0,0,6, &response);
